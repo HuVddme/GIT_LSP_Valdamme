dcfa23ba3ebc7a0cc3bd04e3943faa8b00be827c
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-01-19 09:55:49 -0500

    Hello World!! in java

---
+++ LSP_project_assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+*  Name: Valentine Ezikeoha
+*/
+
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
